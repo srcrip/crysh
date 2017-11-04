e5aee88d17e77dbeda8c5f7e9c14ebfe0bb1be15
sevensidedmarble <andrewianstewart@gmail.com>
2017-11-04 10:23:52 -0400

    added basic builtins

--- src/crysh.cr
+++ src/crysh.cr
@@ -1,15 +1,40 @@
-#!/usr/bin/crystal
 require "./crysh/*"
 
-# module Crysh
-#
-# end
+prompt = "❯ "
 
-STDIN.each_line do |line|
-  pid = Process.fork {
-    Process.exec line
-  }
+def cd(dir)
+  Dir.cd(dir)
+end
 
-  pid.wait
-  # puts line
+def exit(code)
+  if code
+    exit(code.to_i)
+  else
+    exit(0)
+  end
+end
+
+BUILTINS = {
+  "cd"   => ->cd (String),
+  "exit" => ->exit (String),
+}
+
+loop do
+  print prompt
+
+  if (line = gets)
+    line = line.strip
+    args = line.split
+    command = args.shift
+
+    if BUILTINS.has_key? command.to_s
+      BUILTINS[command.to_s].call(args.to_s)
+    else
+      pid = Process.fork {
+        Process.exec line
+      }
+
+      pid.wait
+    end
+  end
 end

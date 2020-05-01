818c9cf6259b5d7549ce9a528a8fc30aa88a0e0f
sevensidedmarble (Andrew Stewart) <andrewianstewart@gmail.com>
2020-05-01 17:00:55 -0400

    The best spec ever

--- spec/crysh_spec.cr
+++ spec/crysh_spec.cr
@@ -1,9 +1,9 @@
 require "./spec_helper"
 
-# describe Crysh do
-#   # TODO: Write tests
+describe Crysh do
+  # TODO: Write tests
 
-#   it "works" do
-#     true.should eq(true)
-#   end
-# end
+  it "works" do
+    true.should eq(true)
+  end
+end
